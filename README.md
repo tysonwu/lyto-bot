7315caf2c5717450075eb0ce2cafafef1a78dcac
TysonW <wukwanyutyson@gmail.com>
2020-05-03 23:36:09 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # lyto-bot
- 
+
+## Introduction
+
+> This script is solely made for educational use for learning `pyautogui`.
+
+This is a little bot which automatically plays the Facebook game *Lyto Different Color*.
+
+A link to the game: https://fb.gg/play/lytodifferentcolor
+
+The main principle is to recognize the minority color in the game window and click on it.
+
+The bot is done by using the Python package of auto-controlling `pyautogui` and image processing `pillow`
+
+
+
+## How to run
+
+- Run the script bot.py 
+
+- Adjust the position of the game window relative to your screen.
+  - May have to re-specify `AREA` 
+- To abort the bot, move the cursor to any of the corner to trigger termination of bot
+
+
+
+## Demonstration
+
+![lyto_demo](/Users/TysonWu/dev/lyto-bot/lyto-bot/lyto_demo.gif)
--- bot.py
+++ bot.py
@@ -2,21 +2,25 @@ import pyautogui
 from collections import Counter
 import time
 import PIL.ImageGrab as ImageGrab
-import sys
+
 pyautogui.FAILSAFE = True
 
-AREA = (132, 977, 868, 1713) 
+# specify game window, the square cropping out the main game area------
+# do not leave too many margin outside the colored circles
+AREA = (132, 977, 868, 1713)
+#----------------------------------------------------------------------
 
 def click(coord): # input a coord tuple (x,y)
     pyautogui.moveTo(coord[0], coord[1], duration=0.01)
 #     screen_size = pyautogui.size()
     pyautogui.click(button='left')
 
+
 def locate():
     box = ImageGrab.grab(AREA)
     h, w = box.size
 #     print(h,w)
-    pixel = list(Image.Image.getdata(box))
+    pixel = list(box.getdata())
     counter = Counter(pixel)
     common = counter.most_common(3)
     pick = common[-1][0]
@@ -25,10 +29,10 @@ def locate():
     for i, pixel in enumerate(pixel):
         if pixel == pick:
             location = i
-    
+
     coord_x = location % w
     coord_y = location // w
-    
+
     click_x = AREA[0] + coord_x
     click_y = AREA[1] + coord_y
     print(location % w, location // w)
@@ -44,7 +48,6 @@ def main():
             i += 1
         except:
             print('fail')
-            sys.exit()
         time.sleep(0.1)
 
 if __name__ == "__main__":
